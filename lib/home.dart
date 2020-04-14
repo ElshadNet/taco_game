73f3df7fb6b2ece6070cfda3fa7ce0d055a3b99a
ElshadNet <elshadnet@gmail.com>
2020-04-14 08:50:31 -0700

    video 14-15-16

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:taco_game/login_page.dart';
 import 'firebase_actions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 class Home extends StatefulWidget {
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_actions.dart';
-import 'user.dart';
-import 'home.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
